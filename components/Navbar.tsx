a480b87e2288b9601edfffedf4d1e8a8dd070a31
Nesandu <nesanduirosha8@gmail.com>
2025-02-25 21:19:26 +0530

    16  commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -61,7 +61,7 @@ const Navbar = async () => {
                                     <form action={
                                         async () => {
                                             'use server';
-                                            await signOut()
+                                            await signOut({ redirectTo: '/' })
 
                                         }
                                     }>
